0ce84f36ecaee617009328609cde1e0c9ddc1455
Hamed <hamed.jenabi@gmail.com>
2020-05-17 10:39:27 +0200

    Weather-SaveBeforeTable

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ export default function App() {
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
   const [weatherSky, setWeatherSky] = useState({});
+
   const api = {
     key: 'ad9e8a41a3b0321cfbc356d29726abff',
     base: 'https://api.openweathermap.org/data/2.5/',
   };
+  const [showInfo, setShowInfo] = useState('none');
 
+  function showDiv() {
+    setShowInfo('block');
+  }
   function responsing() {
     fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
       .then((res) => res.json())
@@ -22,10 +27,12 @@ export default function App() {
         setCity('');
         setWeather(weatherMain);
         setWeatherSky(weatherSkys);
-        console.log(result);
+        // console.log(result);
       });
     alert(`Get ready to see today's weather in ${city}?!`);
+    showDiv();
   }
+
   // useEffect(
   //   function Weather(props) {
   //     // fetch(`${api.base}weather?q=${cityFinal}&units=metric&APPID=${api.key}`)
@@ -41,30 +48,30 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <h1 className="Div">Hamed's Weather App</h1>
-          <hr />
-          Write down your city name to get
-          <h3>current weather </h3>
-          {/* {weather.weather.main.feels_like} */}
-          this is city's
-          <Form
-            city={city}
-            setCity={setCity}
-            onSubmit={function onSubmit(city) {}}
-          />
-          <br />
-          {weather.temp}
-          <br />
-          {weatherSky.description}
-          <button className="button" type="submit" onClick={() => responsing()}>
-            {/* alert('ARE YOU READY TO SEE the weather?'))  */}
-            Submit
-          </button>
-          <div> here we go</div>
-          {/* <Weather /> Here comes the weather */}
+        <h3>
+          Hamed's Weather App <hr />
+        </h3>
+
+        <Form
+          city={city}
+          setCity={setCity}
+          onSubmit={function onSubmit(city) {}}
+        />
+        <br />
+
+        <button className="button" type="submit" onClick={() => responsing()}>
+          {/* alert('ARE YOU READY TO SEE the weather?'))  */}
+          Submit
+        </button>
+        <div className="Answer">
+          <div style={{ display: showInfo }}>
+            weather is bla bla
+            {weatherSky.description}
+          </div>
         </div>
       </header>
+
+      <header />
     </div>
   );
 }
